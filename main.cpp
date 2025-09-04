ebe9dfd8264a700f4ecc3cbbfb47fba4cf423b2f
KevinLicon <licon_kevin@outlook.com>
2025-09-03 20:49:48 -0600

    New Project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a;
+
+    cout << "Hello add a new number: " << endl;
+
+    cin >> a;
+
+    cout << "You added: " << a << endl;
+
+    return 0;
+
+}

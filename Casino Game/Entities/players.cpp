e9fc2e32084b93b8ec07cb8d70603c2924dbd1ee
KevinLicon <licon_kevin@outlook.com>
2025-09-04 21:47:12 -0600

    new layer for players

---
+++ Casino Game/Entities/players.cpp
@@ -0,0 +1,13 @@
+#include<vector>
+#include "player.cpp"
+
+using namespace std;
+
+class Players
+{
+
+    vector<Player> players;
+
+    
+
+};
--- Casino Game/main.cpp
+++ Casino Game/main.cpp
@@ -1,27 +1,42 @@
 #include <iostream>
+#include <vector>
 #include "Entities/player.cpp"
 
 using namespace std;
 
 int main() {
 
+    vector<Player> players ;
 
     int condition = 0;
 
     do
     {
-        
+        system("cls");
+
         cout << "Enter a valid option to play" << endl;
 
         cout << "1: Create a new Player" << endl;
 
         cout << "9: Exist the program" << endl;
 
-        cout << "Select an option [ ";
+        cout << "Select an option: ";
         
         cin >> condition;
 
-        cout << " ]" << endl;
+        cout << endl;
+
+        switch (condition)
+        {
+            case 0:
+                cout << "Helo new palyer " << endl;
+            break;
+        
+            default:
+                cout << "WTF ARE YOU DUM!!" << endl;
+            break;
+        }
+
 
     } while (condition != 9);
 
